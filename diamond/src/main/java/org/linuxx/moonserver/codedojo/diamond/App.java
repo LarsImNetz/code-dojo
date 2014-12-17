924d5621d4903550dac092df16bd9107e09d971c
Lars Langhans <lars.langhans@hypoport.de>
2014-12-17 17:21:20 +0100

    Empty Dojo-Diamond Projekt created

---
+++ diamond/src/main/java/org/linuxx/moonserver/codedojo/diamond/App.java
@@ -0,0 +1,27 @@
+package org.linuxx.moonserver.codedojo.diamond;
+
+/**
+ * Create a Diamond Printer
+ *
+ * Wird gefüttert mit 'A'
+ * und gibt aus:
+ * A
+ * 
+ * Wird gefüttert mit 'B' und gibt aus:
+ *  A
+ * B B
+ *  A
+ *
+ * Wird gefüttert mit 'C' und gibt aus:
+ *   A
+ *  B B
+ * C   C
+ *  B B
+ *   A
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ diamond/src/test/java/org/linuxx/moonserver/codedojo/diamond/TestApp.java
@@ -0,0 +1,12 @@
+package org.linuxx.moonserver.codedojo.diamond;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestApp {
+	@Test
+	public void testApp() {
+		Assert.assertTrue(true);
+	}
+}
